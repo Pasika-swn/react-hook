7b52d5a650a528698fc99be0144d908fb09bbf66
Pasika Sriwanna <pasika.swn@gmail.com>
2024-01-26 14:35:46 +0700

    useEffect()

--- src/CounterPage.js
+++ src/CounterPage.js
@@ -1,18 +1,36 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Wrapper, CounterText, Button } from "./Components";
 
 export const CounterPage = () => {
   const [counter, setCounter] = useState(10);
 
+  useEffect(() => {
+    //ถูกรันตอนครั้งแรกที่ cpn มีการ mount
+    // setInterval(() => {
+    //   setCounter(counter - 1)
+    // }, 1000)
+    //----notice---- there is some bugs (lagging) -> bc โดนรัน interval ซ้ำๆในเวลาเหลื่อมกัน
 
-  
+    //-how to fix-// -> using function instead
+    setInterval(() => {
+      setCounter((previousCounter) => previousCounter > 0 ? previousCounter - 1 : previousCounter);
+    }, 1000);
+  }, []); //กำหนด dependency -> [] -> it should be run just once
 
   return (
     <Wrapper>
       <CounterText>{counter}</CounterText>
       <div>
-        <Button onClick={() => setCounter((previousCounter) => previousCounter -1)}>-1</Button>
-        <Button onClick = {() => setCounter((previousCounter) => previousCounter + 1)}>+1</Button>
+        <Button
+          onClick={() => setCounter((previousCounter) => previousCounter - 1)}
+        >
+          -1
+        </Button>
+        <Button
+          onClick={() => setCounter((previousCounter) => previousCounter + 1)}
+        >
+          +1
+        </Button>
       </div>
     </Wrapper>
   );
